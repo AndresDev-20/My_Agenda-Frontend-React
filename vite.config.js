4511aceef8375563c8b8e787668cdf8bdf483b49
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2024-08-12 22:44:31 -0500

    Version 0.0.1

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    open: "0.0.0.0",
-  },
-  build: {
-    manifest: true,
-  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Agenda',
+        short_name: 'My Agenda',
+        description: 'Mi Aplicación PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './images/logo.png', 
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './images/logo.png', 
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ]
 });
