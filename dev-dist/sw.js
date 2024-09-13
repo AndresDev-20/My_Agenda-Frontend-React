a545ab7e03605a54f8416d1b8a7bd464a96362f8
Yeison Andrés Marroquín Bernal <yeisonmarroquin887@gmail.com>
2024-09-13 15:40:50 -0500

    My Agenda

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.042n04sieeo"
+    "revision": "0.3ulpf6hc7l"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
