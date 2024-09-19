aa7445e62cbda7c22d0a90c6383cf51b8f92967d
Yeison Andrés Marroquín Bernal <yeisonmarroquin887@gmail.com>
2024-09-18 23:50:31 -0500

    Version 0.0.1

--- src/components/Days/Day.jsx
+++ src/components/Days/Day.jsx
@@ -3,9 +3,11 @@ import data from '../../utils/data.json';
 import './Day.css';
 
 const Day = () => {
+  const [selectedDay, setSelectedDay] = useState(null);
   const [Days, setDay] = useState();
 
   const selection = (option) => {
+    setSelectedDay(option); // Actualizar el índice del día seleccionado
     switch (option) {
       case 0:
         setDay(data.days[0]);
@@ -34,12 +36,15 @@ const Day = () => {
   return (
     <div className='Days'>
       <ul className='Days__Menu'>
-        <li onClick={() => selection(0)}>Lunes</li>
-        <li onClick={() => selection(1)}>Martes</li>
-        <li onClick={() => selection(2)}>Miércoles</li>
-        <li onClick={() => selection(3)}>Jueves</li>
-        <li onClick={() => selection(4)}>Viernes</li>
-        <li onClick={() => selection(5)}>Sábado</li>
+        {['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'].map((day, index) => (
+          <li 
+            key={index} 
+            onClick={() => selection(index)} 
+            className={selectedDay === index ? 'active' : ''}  // Añadir clase 'active' si el día está seleccionado
+          >
+            {day}
+          </li>
+        ))}
       </ul>
       <div className='Days__day'>
         {Days ? (
@@ -59,8 +64,9 @@ const Day = () => {
           </>
         ) : (
           <div>
-			<h1 className='Bienvenida'>Bienvenido! Selecciona un día para ver la agenda.</h1>
-		  </div>
+            <h1 className='Bienvenida'>Bienvenido! Selecciona un día para ver la agenda.</h1>
+            <img className='Imagesuto' src="../../../../images/aqendalimg.png" alt="" />
+          </div>
         )}
       </div>
     </div>
