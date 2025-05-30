dc1c54a933910aca3a37e4b2053133a10573415d
AndresDev20 <andresmarroquin887@gmail.com>
2025-05-30 18:06:19 -0500

    commit de mejora

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pg7718iv5g8"
+    "revision": "0.tigh2pc6p7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
